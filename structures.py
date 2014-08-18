dcaa89bee5dcea7299d64975ac1c520909abdc91
Joseph Redfern <joseph@redfern.me>
2014-08-18 21:55:14 +0100

    use crazy enum reverse lookup stuff to determine proper enum value for CPUType

--- structures.py
+++ structures.py
@@ -3,12 +3,13 @@ from enum import Enum
 class MachHeader:
     def __init__(self, magic, cputype, cpusubtype, filetype, ncmds, sizeofcmds, flags):
         self.magic = magic
-        self.cputype = cputype #Eventually CPUType.<appropriate value>
+        self.cputype = self.get_cpu_type(cputype)
         self.cpusubtype = cpusubtype
         self.filetype = filetype
         self.ncmds = ncmds
         self.sizeofcmds = sizeofcmds
         self.flags = flags
+
 
     def __str__(self):
         return "Magic: {0}, CPU Type: {1}, CPU Subtype: {2}, Filetype: {3}, ncmds: {4}, sizeofcmds: {5}, flags: {6}".format(
@@ -29,6 +30,15 @@ class MachHeader:
         CPU_TYPE_POWERPC   = 18
         CPU_TYPE_POWERPC64 = CPU_TYPE_POWERPC | 0x01000000
 
+        def __str__(self):
+            return self.name
+
+
+    def get_cpu_type(self, value):
+        members = {getattr(self.CPUType, member).value : getattr(self.CPUType, member) for member in dir(self.CPUType) if not callable(member) and not member.startswith("__")}
+        return members[value]
+
 class MachoFile:
     def __init__(self, header): #more to come
         self.header = header
+        self.header.get_cpu_type(7)
